8258a28dec046326de9157c890bde34beabaf494
Nuriden Muktar <ratkum.2001@gmail.com>
2024-02-20 20:38:44 +0600

    add annotate and price

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -9,7 +9,10 @@ from services.serializers import SubscriptionsSerializer
 
 # Create your views here.
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all().prefetch_related(Prefetch(
+    queryset = Subscription.objects.all().prefetch_related(
+        'plan',
+        Prefetch(
         'client', queryset=Client.objects.all().select_related('user').only('company_name',
-                                                                            'user__email')))
+                                                                            'user__email'))
+    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionsSerializer
