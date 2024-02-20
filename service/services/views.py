050be8ace4baf16724d546e30c6c55d6afa2eadc
Nuriden Muktar <ratkum.2001@gmail.com>
2024-02-20 15:50:56 +0600

    create serializers and add code views , upgrade url

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from services.models import Subscription
+from services.serializers import SubscriptionsSerializer
+
 
 # Create your views here.
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionsSerializer
