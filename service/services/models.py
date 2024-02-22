42edf1f003b845fadc825dc9724fd4a798b0d839
Nuriden Muktar <ratkum.2001@gmail.com>
2024-02-22 15:57:07 +0600

    add mini celery tasks and add celery_singleton

--- service/services/models.py
+++ service/services/models.py
@@ -2,27 +2,52 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
-
+from services.tasks import set_price
 
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price= models.PositiveIntegerField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 class Plan(models.Model):
-    PLAN_TYPES=(
-        ('full' , 'Full'),
+    PLAN_TYPES = (
+        ('full', 'Full'),
         ('student', 'Student'),
         ('discount', 'Discount')
     )
 
-    plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
     discount_percent = models.PositiveIntegerField(default=0,
                                                    validators=[
                                                        MaxValueValidator(100)
                                                    ])
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
     client=models.ForeignKey(Client, on_delete=models.PROTECT, related_name='subscriptions')
     service=models.ForeignKey(Service, on_delete=models.PROTECT, related_name='subscriptions')
     plan=models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='subscriptions')
+    price = models.PositiveIntegerField(default=0)
